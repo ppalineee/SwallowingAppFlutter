41deec6fcac5a54f2238d789bc4e9a7cc5e553d9
ppalineee <s.palinee98@gmail.com>
2021-01-10 01:40:34 +0700

    Change baseUrl

--- lib/data/network/constants/endpoints.dart
+++ lib/data/network/constants/endpoints.dart
@@ -1,9 +1,10 @@
 class Endpoints {
   Endpoints._();
 
-  static const String baseUrl = "https://testchulasenior.et.r.appspot.com";
+  static const String baseUrl = "https://finaltestserver-301208.et.r.appspot.com";
   static const int receiveTimeout = 8000;
   static const int connectionTimeout = 8000;
   static const String loginPatient = baseUrl + "/loginpatient";
   static const String profile = baseUrl + "/profile";
+  static const String submitScore = baseUrl + "/testdone";
 }
